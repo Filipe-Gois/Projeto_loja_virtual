303040c4f3bee3470664f34e2373c072e157a558
Fernando Quatrochi <fernandinnn145@gmail.com>
2023-04-28 17:17:48 -0300

    Fazendo o debito

---
+++ Debito.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_loja_virtual
+{
+    public class Debito : cartao
+
+
+    {
+        
+    }
+}
--- Pagamento.cs
+++ Pagamento.cs
@@ -8,7 +8,7 @@ namespace Projeto_loja_virtual
     public abstract class Pagamento
     {
         private DateTime Data {get; set;}
-        public float Valor;
+        public float this.Valor;
 
         public string Cancelar(){
             return "Operação Cancelada!";
