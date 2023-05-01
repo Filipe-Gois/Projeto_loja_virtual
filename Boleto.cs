85d386267885a2344f76fa17a22243c7716c400c
Pedro felix <Pedro@gmail.com>
2023-05-01 15:22:48 -0300

    Criado boleto

--- Boleto.cs
+++ Boleto.cs
@@ -1,13 +1,39 @@
+using System.Globalization;
+using System;
+using static System.Globalization.CultureInfo;
+
 namespace Projeto_loja_virtual
 {
     public class Boleto : Pagamento
     {
-        Random CdgBarras = new Random();
+        Random CodigoDeBarras = new Random();
+        // public DateTime Data;
 
+    
         private string CodigoBarras()
         {
-            Console.WriteLine($"0339{CdgBarras.Next(9).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(9).ToString()} {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(90000).ToString()}");
-            return "";
+
+            return $"0339{CodigoDeBarras.Next(9).ToString()}.{CodigoDeBarras.Next(9000).ToString()}5 {CodigoDeBarras.Next(90000).ToString()}.{CodigoDeBarras.Next(9000).ToString()}5 {CodigoDeBarras.Next(90000).ToString()}.{CodigoDeBarras.Next(9000).ToString()}5 {CodigoDeBarras.Next(9).ToString()} {CodigoDeBarras.Next(90000).ToString()}.{CodigoDeBarras.Next(90000).ToString()}";
+        }
+
+
+        public void Registrar()
+        {
+            this.Valor = this.Valor * 0.88f;
+
+            Console.WriteLine($"\n");
+            Console.WriteLine($"Compras efetuadas no boleto tem 12% de desconto!!");
+
+            Console.WriteLine(@$"
+            ____________________________________________________________________________
+            
+            Beneficiário: Projeto Loja Virtual
+            Valor do boleto: {this.Valor.ToString(CultureInfo.GetCultureInfo("pt-BR"))}
+            Código de barras: {CodigoBarras()}
+
+            Seu boleto vence em 3 dias.
+            ____________________________________________________________________________
+            ");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,1 @@
 ﻿
-Random CdgBarras = new Random();
-Console.WriteLine($"0339{CdgBarras.Next(9).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(9000).ToString()}5 {CdgBarras.Next(9).ToString()} {CdgBarras.Next(90000).ToString()}.{CdgBarras.Next(90000).ToString()}");
