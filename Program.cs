aeb7d32e5e6ee868a92018d3901b84efe7abcc05
Pedro Gabriel Felix Da Silva <48746019871@info.local>
2023-05-02 15:14:43 -0300

    Att

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Projeto_loja_virtual;
-
-Boleto teste = new Boleto();
-
-Console.WriteLine($"Insira o valor da compra:");
-teste.Valor = float.Parse(Console.ReadLine()!);
-
-teste.Registrar();
